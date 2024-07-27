7372153fe354fc9643db5eb95d28e153aeaf4414
saranshgupta2711 <saranshkeshari9@gmail.com>
2024-07-27 22:40:30 +0530

    url

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,3 +1,5 @@
 export * from "@prisma/client"
-import {PrismaClient} from "@prisma/client"
- export const prisma= new PrismaClient()
+import {PrismaClient} from "@prisma/client/edge"
+console.log('Initializing Prisma Client...');
+export const prisma = new PrismaClient();
+console.log('Prisma Client initialized:', prisma);
