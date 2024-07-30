b318fb79efaf33e35c039603c94cea364e907aee
saranshgupta2711 <saranshkeshari9@gmail.com>
2024-07-30 10:59:44 +0530

    prisma client err resolved

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,5 @@
 export * from "@prisma/client"
-import {PrismaClient} from "@prisma/client/edge"
+import {PrismaClient} from "@prisma/client"
 console.log('Initializing Prisma Client...');
 export const prisma = new PrismaClient();
 console.log('Prisma Client initialized:', prisma);
