49720fd17e74667bc3da3765d495e8a8092d71dc
saranshgupta2711 <saranshkeshari9@gmail.com>
2024-08-07 21:10:27 +0530

    chaching resolved

--- apps/patients/app/api/getDoctors/route.ts
+++ apps/patients/app/api/getDoctors/route.ts
@@ -1,53 +1,14 @@
 import prisma  from "@repo/db/client";
 import { NextResponse } from "next/server";
 
-interface appoint{
-    id        :  number,
-    Purpose   :  string,
-    location  :  string,
-    number    :  string,
-    email     :  string,
-    time      :  string,
-    date      :  string,
-    doctorId  :  number,      
-    userId    :  number,
-    Status    :  string,
-    easeOfUse :   number,
-    worthForMoney: number
-  }
-  interface Doc{
-    image: string,
-    id: number,
-    name: string,
-    email:string,
-    password: string,
-    address: string,
-    phoneNumber: string,
-    age: string,
-    clinic: string,
-    yearOfExp:string,
-    specialisation:string,
-    gender:string, 
-    online:string,
-    offline:string,
-    days:string[],
-    time:string[]
-    appoint: appoint[]
-  } 
-  
-export async function GET():Promise<void | Response>{
+
+export async function GET(){
     
-    try {
        const doctors = await prisma.doctor.findMany({
          include: {
            appoint: true,
          },
        });
        return NextResponse.json({doctors});
-     } catch (error) {
-       console.error("Error fetching doctors:", error);
-       return new Response("Internal Server Error", { status: 500 });
-    }
-   
-
+  
 }
