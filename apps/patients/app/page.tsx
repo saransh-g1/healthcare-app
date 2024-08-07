3030dd270bf879a2a2820bf0e202b0b98cfe3a0d
saranshgupta2711 <saranshkeshari9@gmail.com>
2024-08-08 00:22:24 +0530

    aded no chaching

--- apps/patients/app/page.tsx
+++ apps/patients/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
 
         <div className="flex items-center ">
         <Link href="/signin" className="mr-14 text-lg font-thin" >Login</Link>
-          <Link  href="/signin"className="mr-5 text-lg  font-thin" >Register</Link>
+          <Link  href="/signup"className="mr-5 text-lg  font-thin" >Register</Link>
         </div>
      </div>
 
@@ -27,7 +27,7 @@ export default async function Home() {
       <p className="text-3xl font-black my-1">Avoid Hasseles & delays</p>
       <p className="text-sm my-1">how is healthy today sounds like not good!</p>
       <p className="text-sm my-1">dont worry.Find your doctor online and book as you wish with tryst!</p>
-      <button className="font-bold text-lg w-60 h-10 rounded-md bg-blue-500 my-2">Make An Appointment</button>
+      <Link href="/signup" className="font-bold text-lg w-60 h-10 rounded-md bg-blue-500 my-2">Make An Appointment</Link>
       </div>
     </div>
 
